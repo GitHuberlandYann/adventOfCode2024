cd5634bf7a1411cdbac024a18469871458cc3707
Yann Huberland <yhuberla@fu-r3-p8.s19.be>
2024-12-02 11:00:10 +0100

    day02

--- day02.cpp
+++ day02.cpp
@@ -9,6 +9,21 @@
 
 using namespace std;
 
+static bool check( vector<int> reports, size_t avoid )
+{
+    bool increasing;
+    if (!avoid) reports[0] = reports[1], avoid = 1, increasing = reports[2] > reports[1];
+    else if (avoid == 1) increasing = reports[2] > reports[0];
+    else increasing = reports[1] > reports[0];
+    for (size_t i = 0; i < reports.size() - 1; ++i) {
+        if (i + 1 == avoid) { reports[i + 1] = reports[i]; continue ; }
+        if ((reports[i + 1] > reports[i]) != increasing) return false;
+        int diff = abs(reports[i + 1] - reports[i]);
+        if (!diff || diff > 3) return false;
+    }
+    return true;
+}
+
 static bool solve( string str )
 {
     istringstream in(str);
@@ -20,13 +35,8 @@ static bool solve( string str )
     }
     reports.pop_back();
     if (reports.empty()) return false;
-    bool increasing = reports[1] > reports[0];
-    for (size_t i = 0; i < reports.size() - 1; ++i) {
-        if ((reports[i + 1] > reports[i]) != increasing) return false;
-        int diff = abs(reports[i + 1] - reports[i]);
-        if (!diff || diff > 3) return false;
-    }
-    return true;
+    for (size_t i = 0; i < reports.size(); ++i) if (check(reports, i)) return true;
+    return false;
 }
 
 int main( void )
