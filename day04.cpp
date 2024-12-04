03106b48548d2b081d05238573f1957624fb4c82
GitHuberlandYann <yann.hub@hotmail.com>
2024-12-04 12:26:11 +0100

    day04

--- day04.cpp
+++ day04.cpp
@@ -7,14 +7,15 @@
 #define CUT(x)
 
 using namespace std;
-const string clue = "XMAS";
-const int len = 4;
+const string clue = "MAS";
+const int len = 3;
 int res, w, h;
 string grid;
 
 
 static bool canGo( int x, int y, int dx, int dy )
 {
+    if (x < 0 || x >= w || y < 0 || y >= h) return false;
     if (x + dx * (len - 1) < 0 || x + dx * (len - 1) >= w || y + dy * (len - 1) < 0 || y + dy * (len - 1) >= h) return false;
     for (int i = 0; i < len; x += dx, y += dy, ++i) {
         if (grid[x + y * w] != clue[i]) return false;
@@ -26,14 +27,12 @@ static void solve( void )
 {
     for (int i = 0; i < h; ++i) {
         for (int j = 0; j < w; ++j) {
-            if (canGo(j, i, 1, 0)) ++res;
-            if (canGo(j, i, -1, 0)) ++res;
-            if (canGo(j, i, 0, 1)) ++res;
-            if (canGo(j, i, 0, -1)) ++res;
-            if (canGo(j, i, 1, -1)) ++res;
-            if (canGo(j, i, 1, 1)) ++res;
-            if (canGo(j, i, -1, -1)) ++res;
-            if (canGo(j, i, -1, 1)) ++res;
+			int cnt = 0;
+            if (canGo(j - 1, i + 1, 1, -1)) ++cnt;
+            if (canGo(j - 1, i - 1, 1, 1)) ++cnt;
+            if (cnt < 2 && canGo(j + 1, i + 1, -1, -1)) ++cnt;
+            if (cnt < 2 && canGo(j + 1, i - 1, -1, 1)) ++cnt;
+			if (cnt == 2) ++res;
         }
     }
 }
