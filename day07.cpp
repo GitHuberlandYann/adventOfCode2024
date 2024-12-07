54c4fffad3adb3a06c82c755b77991fc87cdbb2b
GitHuberlandYann <yann.hub@hotmail.com>
2024-12-07 13:07:54 +0100

    day07 - part 1

---
+++ day07.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define OUT(x) cout << x
+#define LOG(x) cerr << x
+#define LOGND(x) cerr << x << endl
+#define CUT(x)
+
+using namespace std;
+
+static bool valid( long total, long curr, int index, int len, vector<long>& factors )
+{
+	if (curr > total) return false;
+	if (index == len) return curr == total;
+	return valid(total, curr * factors[index], index + 1, len, factors)
+		|| valid(total, curr + factors[index], index + 1, len, factors);
+}
+
+int main( void )
+{
+	long res = 0;
+    while (!cin.eof()) {
+        string line;
+		long total, value;
+		cin >> total;
+		cin.ignore(1, ' ');
+		vector<long> factors;
+		while (!cin.eof() && cin.peek() != '\n') cin >> value, factors.push_back(value);
+        if (valid(total, factors.front(), 1, factors.size(), factors)) res += total;
+    }
+
+    OUT(res << endl);
+}
