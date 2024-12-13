4cb1ad2cfa7f498ea556c5157ce884f9fbfeba96
Yann Huberland <yhuberla@fu-r3-p8.s19.be>
2024-12-13 11:20:27 +0100

    day13 - part 1

---
+++ day13.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+#define OUT(x) cout << x
+#define LOG(x) cerr << x
+#define LOGND(x) cerr << x << endl
+#define CUT(x)
+
+using namespace std;
+
+static array<int, 2> readInput( char sep )
+{
+    int x, y;
+    string ign;
+    getline(cin, ign, sep);
+    cin >> x;
+    getline(cin, ign, sep);
+    cin >> y;
+    cin.ignore();
+    cin.ignore();
+    return {x, y};
+}
+
+int main( void )
+{
+    int res = 0;
+    while (!cin.eof()) {
+        auto buttonA = readInput('+');
+        auto buttonB = readInput('+');
+        auto target = readInput('=');
+        if ((target[0] * buttonA[1] - target[1] * buttonA[0]) % (buttonB[0] * buttonA[1] - buttonB[1] * buttonA[0])) continue ;
+        int cntB = (target[0] * buttonA[1] - target[1] * buttonA[0]) / (buttonB[0] * buttonA[1] - buttonB[1] * buttonA[0]);
+        if ((target[1] - cntB * buttonB[1]) % buttonA[1]) continue ;
+        int cntA = (target[1] - cntB * buttonB[1]) / buttonA[1];
+        if (cntA > 100 || cntB > 100) continue ;
+        res += cntA * 3 + cntB;
+        /*
+        cntA * buttonA[0] + cntB * buttonB[0] = target[0];
+        cntA * buttonA[1] + cntB * buttonB[1] = target[1];
+        <=> cntA = (target[1] - cntB * buttonB[1]) / buttonA[1];
+        <=> (target[1] - cntB * buttonB[1]) / buttonA[1] * buttonA[0] + cntB * buttonB[0] = target[0];
+        <=> cntB * buttonB[0] - cntB * buttonB[1] * buttonA[0] / buttonA[1] = target[0] - target[1] * buttonA[0] / buttonA[1];
+        <=> cntB * buttonB[0] * buttonA[1] - cntB * buttonB[1] * buttonA[0] = target[0] * buttonA[1] - target[1] * buttonA[0];
+        <=> cntB = (target[0] * buttonA[1] - target[1] * buttonA[0]) / (buttonB[0] * buttonA[1] - buttonB[1] * buttonA[0]);
+        */
+    }
+
+    OUT(res << endl);
+}
