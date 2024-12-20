9f1e9f3086c776b0570e4f95079f4a669763b048
GitHuberlandYann <yann.hub@hotmail.com>
2024-12-20 17:30:49 +0100

    day20

--- day20.cpp
+++ day20.cpp
@@ -30,10 +30,22 @@ static void genShortCuts( int x, int y, int steps )
 {
 	if (grid[x + y * width] == '#') return ;
 	grid[x + y * width] = '#';
-	res += (x > 1 && grid[x - 1 + y * width] == '#' && grid[x - 2 + y * width] != '#' && dists[x - 2 + y * width] + steps < normalDist - 101)
-		+ (x < width - 2 && grid[x + 1 + y * width] == '#' && grid[x + 2 + y * width] != '#' && dists[x + 2 + y * width] + steps < normalDist - 101)
-		+ (y > 1 && grid[x + (y - 1) * width] == '#' && grid[x + (y - 2) * width] != '#' && dists[x + (y - 2) * width] + steps < normalDist - 101)
-		+ (y < height - 2 && grid[x + (y + 1) * width] == '#' && grid[x + (y + 2) * width] != '#' && dists[x + (y + 2) * width] + steps < normalDist - 101);
+	int minValue = normalDist;
+	for (int dx = -20; dx < 21; ++dx) {
+		if (x + dx < 1 || x + dx >= width - 1) continue ;
+		for (int dy = -20; dy < 21; ++dy) {
+			if (y + dy < 1 || y + dy >= height - 1) continue ;
+			if (abs(dx) + abs(dy) > 20) continue ;
+			if (grid[x + dx + (y + dy) * width] == '#') continue ;
+			if (dists[x + dx + (y + dy) * width] + steps + abs(dx) + abs(dy) <= normalDist - 100) ++res;
+			//if (dists[x + dx + (y + dy) * width] < minValue) minValue = dists[x + dx + (y + dy) * width];
+		}
+	}
+	//if (minValue + steps < normalDist - 51) ++res;
+	//res += (x > 1 && grid[x - 1 + y * width] == '#' && grid[x - 2 + y * width] != '#' && dists[x - 2 + y * width] + steps < normalDist - 101)
+	//	+ (x < width - 2 && grid[x + 1 + y * width] == '#' && grid[x + 2 + y * width] != '#' && dists[x + 2 + y * width] + steps < normalDist - 101)
+	//	+ (y > 1 && grid[x + (y - 1) * width] == '#' && grid[x + (y - 2) * width] != '#' && dists[x + (y - 2) * width] + steps < normalDist - 101)
+	//	+ (y < height - 2 && grid[x + (y + 1) * width] == '#' && grid[x + (y + 2) * width] != '#' && dists[x + (y + 2) * width] + steps < normalDist - 101);
 	genShortCuts(x - 1, y, steps + 1);
 	genShortCuts(x + 1, y, steps + 1);
 	genShortCuts(x, y - 1, steps + 1);
