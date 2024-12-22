4dc2925db3b3b447dfbe0a4c38123d091c682e21
GitHuberlandYann <yann.hub@hotmail.com>
2024-12-22 21:36:07 +0100

    day22

--- day22.cpp
+++ day22.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <map>
 #include <math.h>
 
 #define OUT(x) cout << x
@@ -18,11 +19,24 @@ static void genSecret( long& secret )
 
 int main( void )
 {
-    long res = 0, secret;
+    long res = 0, secret, index = 0;
+	map<array<int, 4>, array<int, 2000>> codex;
     while (!cin.eof()) {
         cin >> secret; cin.ignore();
-		for (int i = 0; i < 2000; ++i) genSecret(secret);
-		res += secret;
+		vector<int> digits{int(secret % 10)};
+		for (int i = 0; i < 2000; ++i) genSecret(secret), digits.push_back(secret % 10);
+		for (int i = 4; i < 2001; ++i) {
+			array<int, 4> key{digits[i - 3] - digits[i - 4], digits[i - 2] - digits[i - 3], digits[i - 1] - digits[i - 2], digits[i] - digits[i - 1]};
+			int curr = digits[i];
+			if (!codex[key][index]) codex[key][index] = curr;
+		}
+		++index;
+	}
+
+	for (auto& [key, vec] : codex) {
+		long sum = 0;
+		for (int i = 0; i < index; ++i) sum += vec[i];
+		res = sum > res ? sum : res;
 	}
 
     OUT(res << endl);
