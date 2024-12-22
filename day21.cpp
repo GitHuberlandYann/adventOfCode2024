74ad614273663cd37a4188ea560c4fc25b21be19
GitHuberlandYann <yann.hub@hotmail.com>
2024-12-22 11:19:37 +0100

    day21

--- day21.cpp
+++ day21.cpp
@@ -10,7 +10,8 @@
 #define CUT(x)
 
 using namespace std;
-vector<string> sequences;
+int shortestSeq;
+string nextSequence;
 const map<char, array<int, 2>> numKeypadCoords{
 	{'7', {0, 0}}, {'8', {1, 0}}, {'9', {2, 0}},
 	{'4', {0, 1}}, {'5', {1, 1}}, {'6', {2, 1}},
@@ -21,53 +22,70 @@ const map<char, array<int, 2>> dirKeypadCoords{
 	{' ', {0, 0}}, {'^', {1, 0}}, {'A', {2, 0}},
 	{'<', {0, 1}}, {'v', {1, 1}}, {'>', {2, 1}}
 };
+const map<char, const map<char, string>> shortestMoves{
+	{'A', { {'A', "A"}, {'^', "<A"}, {'<', "v<<A"}, {'v', "<vA"}, {'>', "vA"} }},
+	{'^', { {'A', ">A"}, {'^', "A"}, {'<', "v<A"}, {'v', "vA"}, {'>', "v>A"} }},
+	{'<', { {'A', ">>^A"}, {'^', ">^A"}, {'<', "A"}, {'v', ">A"}, {'>', ">>A"} }},
+	{'v', { {'A', "^>A"}, {'^', "^A"}, {'<', "<A"}, {'v', "A"}, {'>', ">A"} }},
+	{'>', { {'A', "^A"}, {'^', "<^A"}, {'<', "<<A"}, {'v', "<A"}, {'>', "A"} }},
+};
 
-static void genNumericKeypad( string& code, int index, array<int, 2> pos, array<int, 2> target, string seq, bool dir )
+static bool genNumericKeypad( string& code, int index, array<int, 2> pos, array<int, 2> target, string seq, bool dir )
 {
 	if (pos == target) {
 		if (!seq.empty()) {
 			int changeCnt = 0, prev = 'A';
 			for (int idx = seq.size() - 1; idx >= 0 && seq[idx] != 'A'; prev = seq[idx--]) if (seq[idx] != prev) ++changeCnt;
-			if (changeCnt > 2) return ;
+			if (changeCnt > 2) return false;
 		}
 		seq += 'A';
 		if (!code[++index]) {
-			sequences.push_back(seq);
-			return ;
+			shortestSeq = seq.size();
+			nextSequence = seq;
+			return true;
 		}
 		return genNumericKeypad(code, index, pos, (dir ? dirKeypadCoords : numKeypadCoords).at(code[index]), seq, dir);
 	}
-	if (pos == (dir ? dirKeypadCoords : numKeypadCoords).at(' ')) return ;
-	if (pos[0] > target[0]) genNumericKeypad(code, index, {pos[0] - 1, pos[1]}, target, seq + '<', dir);
-	if (pos[0] < target[0]) genNumericKeypad(code, index, {pos[0] + 1, pos[1]}, target, seq + '>', dir);
-	if (pos[1] > target[1]) genNumericKeypad(code, index, {pos[0], pos[1] - 1}, target, seq + '^', dir);
-	if (pos[1] < target[1]) genNumericKeypad(code, index, {pos[0], pos[1] + 1}, target, seq + 'v', dir);
+	if (pos == (dir ? dirKeypadCoords : numKeypadCoords).at(' ')) return false;
+	if (pos[0] > target[0] && genNumericKeypad(code, index, {pos[0] - 1, pos[1]}, target, seq + '<', dir)) return true;
+	if (pos[1] > target[1] && genNumericKeypad(code, index, {pos[0], pos[1] - 1}, target, seq + '^', dir)) return true;
+	if (pos[1] < target[1] && genNumericKeypad(code, index, {pos[0], pos[1] + 1}, target, seq + 'v', dir)) return true;
+	if (pos[0] < target[0] && genNumericKeypad(code, index, {pos[0] + 1, pos[1]}, target, seq + '>', dir)) return true;
+	return false;
 }
 
-static int solve( string& code )
+map<int, map<char, map<char, pair<long, char>>>> memory;
+static long genKeypad( int i, char start, char end )
 {
+	if (i == 25) return 1;
+	if (memory[i][start].count(end)) { return memory[i][start][end].first; }
+	long res = 0;
+	char curr = 'A';
+	for (auto val : shortestMoves.at(start).at(end)) res += genKeypad(i + 1, curr, val), curr = val;
+	memory[i][start][end] = {res, curr};
+	return res;
+}
+
+static long solve( string& code )
+{
+	nextSequence.clear();
 	genNumericKeypad(code, 0, numKeypadCoords.at('A'), numKeypadCoords.at(code[0]), "", false);
-	auto allSequences = sequences;
-	sequences.clear();
-	for (auto& seq : allSequences) LOGND(seq);
-	for (auto& seq : allSequences) genNumericKeypad(seq, 0, dirKeypadCoords.at('A'), dirKeypadCoords.at(seq[0]), "", true);
-	allSequences = sequences;
-	sequences.clear();
-	for (auto& seq : allSequences) LOGND('\t' << seq);
-	for (auto& seq : allSequences) genNumericKeypad(seq, 0, dirKeypadCoords.at('A'), dirKeypadCoords.at(seq[0]), "", true);
-	allSequences = sequences;
-	sequences.clear();
-	int res = 2147483647;
-	for (auto& seq : allSequences) if (LOGND("\t\t" << seq), seq.size() < res) res = seq.size();
+
+	char curr = 'A';
+	long res = 0;
+	string seq = nextSequence;
+	nextSequence.clear();
+	for (auto val : seq) res += genKeypad(0, curr, val), curr = val;
+	LOGND(code << " -> " << res << " -> " << nextSequence);
 	return res;
 }
 
 int main( void )
 {
 	string code;
-	int res = 0;
+	long res = 0;
     while (getline(cin, code)) {
-		LOGND("solving " << code);
+		OUT("solving " << code << endl);
 		res += solve(code) * atoi(code.c_str());
 	}
 
